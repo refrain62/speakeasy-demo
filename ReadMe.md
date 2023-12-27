6268866326cfe2581e4d1638b826fcac6cc33f05
refrain62 <nao_h13@hotmail.com>
2023-12-27 12:22:51 +0900

    first commit

---
+++ ReadMe.md
@@ -0,0 +1,9 @@
+# speakeasyのデモ
+https://github.com/speakeasyjs/speakeasy-demo の写経
+
+
+## 依存関係の追加
+```
+npm install speakeasy qr-image express express-tl nodemon
+```
+
---
+++ index.js
@@ -0,0 +1,25 @@
+const speakeasy = require('speakeasy')
+const qr = require('qr-image')
+const express = require('express')
+const tl = require('express-tl')
+
+const app = express();
+
+// viewのテンプレートエンジン設定
+app.engine('tl', tl)
+app.set('views', './views')
+app.set('view engine', 'tl')
+
+// トップページ
+app.get('/', function(req, res) {
+  // レンダリング
+  res.render('index', {
+    // パラメータ
+    sport: true
+  })
+})
+
+// リスナー起動
+app.listen(3000, () => {
+  console.log('Express listening on port 3000!')
+})
